8c92fea81fc168cfe228ea2167baf67fef3837b6
Sudip Baral <sudip.baral@Sudips-MacBook-Pro.local>
2023-10-19 00:48:30 -0700

    added favicon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import { Provider } from 'react-redux';
 import store from './store'
 import Count from './home/count';
+import { AboutUs } from './home/about-us';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Count />
       </div>
+      <AboutUs />
     </Provider>
 
   );
---
+++ src/home/about-us.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from "react"
+
+export const AboutUs: FC = () => {
+    return <div>About us test page</div>
+}
